4a99122cfd1bf59098e90dcd863d7910c9f83442
Matheus Richard <matheusrichardt@gmail.com>
2021-02-28 01:03:29 -0300

    Add string colors

--- lib/rich_engine.rb
+++ lib/rich_engine.rb
@@ -3,6 +3,7 @@
 require_relative 'rich_engine/canvas'
 require_relative 'rich_engine/cooldown'
 require_relative 'rich_engine/game'
+require_relative 'rich_engine/string_colors'
 require_relative 'rich_engine/timer'
 require_relative 'rich_engine/version'
 
---
+++ lib/rich_engine/string_colors.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module RichEngine
+  module StringColors
+    refine String do
+      def black
+        color(30)
+      end
+
+      def red
+        color(31)
+      end
+
+      def green
+        color(32)
+      end
+
+      def brown
+        color(33)
+      end
+
+      def blue
+        color(34)
+      end
+
+      def magenta
+        color(35)
+      end
+
+      def cyan
+        color(36)
+      end
+
+      def white
+        color(37)
+      end
+
+      private
+
+      def color(n)
+        "\e[#{n}m#{self}\e[0m"
+      end
+    end
+  end
+end
