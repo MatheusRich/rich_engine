43ad7d96c5d572d0c4ecfd2bb81683e7ac6cf04f
Matheus Richard <matheusrichardt@gmail.com>
2021-03-02 21:51:49 -0300

    Update example

--- test/example.rb
+++ test/example.rb
@@ -2,20 +2,23 @@
 
 require 'rich_engine'
 
-class MyGame < RichEngine::Game
+class TimerExample < RichEngine::Game
   def on_create
     @timer = RichEngine::Timer.new
+    @canvas.bg = '.'
   end
 
   def on_update(elapsed_time, key)
     game_over = key == :q
 
     @timer.update(elapsed_time)
+    @canvas.clear
     @canvas.write_string("Elapsed: #{@timer.get.round(1)}s", x: 1, y: 1)
     game_over = true if @timer.get > 10
+    @io.write(@canvas.canvas)
 
     !game_over
   end
 end
 
-MyGame.play
+TimerExample.play
