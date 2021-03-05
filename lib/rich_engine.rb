7668b660254ee982f8d1d3023ee7b583bf89d387
Matheus Richard <matheusrichardt@gmail.com>
2021-03-05 19:23:21 -0300

    Add Vec2 class

--- lib/rich_engine.rb
+++ lib/rich_engine.rb
@@ -4,8 +4,9 @@ require_relative 'rich_engine/canvas'
 require_relative 'rich_engine/cooldown'
 require_relative 'rich_engine/game'
 require_relative 'rich_engine/string_colors'
-require_relative 'rich_engine/timer'
 require_relative 'rich_engine/terminal'
+require_relative 'rich_engine/timer'
+require_relative 'rich_engine/vec2'
 require_relative 'rich_engine/version'
 
 module RichEngine
---
+++ lib/rich_engine/vec2.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module RichEngine
+  class Vec2
+    def initialize(width:, height:, fill_with:)
+      @vec = Array.new(width) { Array.new(height) { fill_with } }
+    end
+
+    def [](x, y)
+      @vec[x][y]
+    end
+
+    def []=(x, y, value)
+      @vec[x][y] = value
+    end
+
+    def each
+      @vec.each do |row|
+        row.each do |tile|
+          yield(tile)
+        end
+      end
+    end
+
+    def each_with_indexes
+      @vec.each_with_index do |row, i|
+        row.each_with_index do |tile, j|
+          yield(tile, i, j)
+        end
+      end
+    end
+
+    def fill(x:, y:, with:)
+      xs = make_iterable(x)
+      ys = make_iterable(y)
+
+      xs.each do |x|
+        ys.each do |y|
+          @vec[x][y] = with
+        end
+      end
+    end
+
+    private
+
+    def make_iterable(value)
+      value.respond_to?(:each) ? value : [value]
+    end
+  end
+end
