ca1b7898a7b0913d596ae162360f04f9e78073d1
Matheus Richard <matheusrichardt@gmail.com>
2021-10-19 12:28:57 -0300

    Make Chance.of work with values lower than 1
    This allows using it like this
    
    ```
    if Chance.of(10/100)
      lucky!
    else
      sorry!
    end
    ```

--- test/rich_engine/chance_test.rb
+++ test/rich_engine/chance_test.rb
@@ -3,11 +3,19 @@
 require "test_helper"
 
 class ChanceTest < Minitest::Test
-  def test_chance_of
+  def test_chance_of_with_values_greater_than_1
     assert_equal false, RichEngine::Chance.of(50, rand_gen: rand_mock(returns: 0.6))
     assert_equal true, RichEngine::Chance.of(50, rand_gen: rand_mock(returns: 0.4))
     assert_equal false, RichEngine::Chance.of(50, rand_gen: rand_mock(returns: 0.5))
     assert_equal true, RichEngine::Chance.of(100)
+    assert_equal false, RichEngine::Chance.of(0)
+  end
+
+  def test_that_with_values_lower_than_1
+    assert_equal false, RichEngine::Chance.of(0.5, rand_gen: rand_mock(returns: 0.6))
+    assert_equal true, RichEngine::Chance.of(0.5, rand_gen: rand_mock(returns: 0.4))
+    assert_equal false, RichEngine::Chance.of(0.5, rand_gen: rand_mock(returns: 0.5))
+    assert_equal true, RichEngine::Chance.of(1)
     assert_equal false, RichEngine::Chance.of(0)
   end
 
