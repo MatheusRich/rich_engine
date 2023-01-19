fd041529767235bb4dfae7b2517c29fd7bffec6f
Matheus Richard <matheusrichardt@gmail.com>
2023-01-19 13:50:27 -0300

    Fix example

--- test/example.rb
+++ test/example.rb
@@ -9,15 +9,14 @@ class TimerExample < RichEngine::Game
   end
 
   def on_update(elapsed_time, key)
-    game_over = key == :q
+    quite! if key == :q
 
     @timer.update(elapsed_time)
     @canvas.clear
     @canvas.write_string("Elapsed: #{@timer.get.round(1)}s", x: 1, y: 1)
-    game_over = true if @timer.get > 10
-    @io.write(@canvas.canvas)
 
-    !game_over
+
+    quit! if @timer.get > 10
   end
 end
 
